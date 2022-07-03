846520f25a7f00d806470ceeca549a73e31e6798
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-07-03 08:06:59 +0100

    cteating database and two tables

---
+++ one-to-one/main.py
@@ -0,0 +1,49 @@
+from sqlalchemy import ForeignKey, create_engine, Column, String, Integer
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+connection_string = 'sqlite:///' + os.path.join(BASE_DIR, 'data.db')
+
+#base class
+Base = declarative_base()
+
+#create and engine
+engine = create_engine(connection_string, echo=True)
+
+#create a connection class
+Session = sessionmaker()
+
+"""
+   class Parent:
+      id:int pk
+      name:str
+
+
+   class Child:
+      id:int pk
+      name:str
+      parent_id:int fk (parent)
+"""
+
+class Parent(Base):
+   __tablename__ = 'parents'
+   id=Column(Integer(), primary_key=True)
+   name=Column(String(25), nullable=False)
+   child = relationship('Child', back_populates='parent', uselist=False)
+
+   def __repr__(self) -> str:
+      return f"<Parent {self.id}>"
+
+class Child(Base):
+   __tablename__ = 'children'
+   id=Column(Integer(), primary_key=True)
+   name=Column(String(25), nullable=False)
+   parent_id=Column(Integer(), ForeignKey('parents.id'))
+   parent = relationship('Parent', back_populates='child')
+
+   def __repr__(self) -> str:
+      return f"<Child {self.id}>"
+   
+Base.metadata.create_all(engine)
